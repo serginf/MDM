813de4f1599187c777f41bcbb2d5d1b120b08199
Alba <alba.canete.garrucho@est.fib.upc.edu>
2018-02-05 11:17:21 +0100

    draw line

--- MetadataFrontend/public/js/client_view_queries_d3.js
+++ MetadataFrontend/public/js/client_view_queries_d3.js
@@ -54,7 +54,7 @@ $(window).load(function() {
             .style('opacity',0)
             .attr('width', "100%")
             .attr('height', "100%")
-            .call(d3.behavior.zoom().on("zoom", redraw));
+            .call(d3.zoom().on("zoom", redraw))
 
         var svg = outer
             .append('g')
@@ -182,10 +182,12 @@ $(window).load(function() {
             Based in Mike Bostock's Line drawing
             https://bl.ocks.org/mbostock/f705fc55e6f26df29354
          */
-       /* var line = d3.svg.line()
-            .curve(d3.curveBasis);
+        var line = d3.line();
 
-        var svg = d3.select("svg")
+        var svg = d3.select("#sourceLevelWrapper").append("svg")
+            .attr("width", $(window).width()*0.75)
+            .attr("height", $(window).height()*0.75)
+            .attr("pointer-events", "all")
             .call(d3.drag()
                 .container(function() { return this; })
                 .subject(function() { var p = [d3.event.x, d3.event.y]; return [p, p]; })
@@ -193,7 +195,10 @@ $(window).load(function() {
 
         function dragstarted() {
             var d = d3.event.subject,
-                active = svg.append("path").datum(d),
+                active = svg.append("path").datum(d)
+                    .attr("stroke", "#ff3f27")
+                    .attr("fill", "none")
+                    .attr("stroke-width", 3),
                 x0 = d3.event.x,
                 y0 = d3.event.y;
 
@@ -207,7 +212,102 @@ $(window).load(function() {
                 else d[d.length - 1] = [x1, y1];
                 active.attr("d", line);
             });
-        }*/
+        }
 
+
+        /*
+            Based in Imad Boumzaoued's https://gist.github.com/RiseupDev/b07f7ccc1c499efc24e9
+         */
+      /*  var dragging = false, drawing = false, startPoint;
+        var points = [], g;
+    // behaviors
+        var dragger = d3.behavior.drag()
+            .on('drag', handleDrag)
+            .on('dragend', function(d){
+                dragging = false;
+            });
+        svg.on('mouseup', function(){
+            if(dragging) return;
+            drawing = true;
+            startPoint = [d3.mouse(this)[0], d3.mouse(this)[1]];
+            if(svg.select('g.drawPoly').empty()) g = svg.append('g').attr('class', 'drawPoly');
+            if(d3.event.target.hasAttribute('is-handle')) {
+                closePolygon();
+                return;
+            };
+            points.push(d3.mouse(this));
+            g.select('polyline').remove();
+            var polyline = g.append('polyline').attr('points', points)
+                .style('fill', 'none')
+                .attr('stroke', '#000');
+            for(var i = 0; i < points.length; i++) {
+                g.append('circle')
+                    .attr('cx', points[i][0])
+                    .attr('cy', points[i][1])
+                    .attr('r', 4)
+                    .attr('fill', 'yellow')
+                    .attr('stroke', '#000')
+                    .attr('is-handle', 'true')
+                    .style({cursor: 'pointer'});
+            }
+        });
+        function closePolygon() {
+            svg.select('g.drawPoly').remove();
+            var g = svg.append('g');
+            g.append('polygon')
+                .attr('points', points)
+                .style('fill', getRandomColor());
+            for(var i = 0; i < points.length; i++) {
+                var circle = g.selectAll('circles')
+                    .data([points[i]])
+                    .enter()
+                    .append('circle')
+                    .attr('cx', points[i][0])
+                    .attr('cy', points[i][1])
+                    .attr('r', 4)
+                    .attr('fill', '#FDBC07')
+                    .attr('stroke', '#000')
+                    .attr('is-handle', 'true')
+                    .style({cursor: 'move'})
+                    .call(dragger);
+            }
+            points.splice(0);
+            drawing = false;
+        }
+        svg.on('mousemove', function() {
+            if(!drawing) return;
+            var g = d3.select('g.drawPoly');
+            g.select('line').remove();
+            var line = g.append('line')
+                .attr('x1', startPoint[0])
+                .attr('y1', startPoint[1])
+                .attr('x2', d3.mouse(this)[0] + 2)
+                .attr('y2', d3.mouse(this)[1])
+                .attr('stroke', '#53DBF3')
+                .attr('stroke-width', 1);
+        })
+        function handleDrag() {
+            if(drawing) return;
+            var dragCircle = d3.select(this), newPoints = [], circle;
+            dragging = true;
+            var poly = d3.select(this.parentNode).select('polygon');
+            var circles = d3.select(this.parentNode).selectAll('circle');
+            dragCircle
+                .attr('cx', d3.event.x)
+                .attr('cy', d3.event.y);
+            for (var i = 0; i < circles[0].length; i++) {
+                circle = d3.select(circles[0][i]);
+                newPoints.push([circle.attr('cx'), circle.attr('cy')]);
+            }
+            poly.attr('points', newPoints);
+        }
+        function getRandomColor() {
+            var letters = '0123456789ABCDEF'.split('');
+            var color = '#';
+            for (var i = 0; i < 6; i++) {
+                color += letters[Math.floor(Math.random() * 16)];
+            }
+            return color;
+        }*/
     });
 });
