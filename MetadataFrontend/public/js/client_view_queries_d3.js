f5be9fd834b2782a550dc1b079965e2cdd76240a
Alba <alba.canete.garrucho@est.fib.upc.edu>
2018-02-10 22:56:14 +0100

    draw line

--- MetadataFrontend/public/js/client_view_queries_d3.js
+++ MetadataFrontend/public/js/client_view_queries_d3.js
@@ -11,6 +11,24 @@ function euclidean(xA,yA,xB,yB) {
 
 $(window).load(function() {
 
+    var width = $(window).width()*0.75;
+    var height = $(window).height()*0.75;
+    var nodeRadius = 9;
+
+   /* outer = d3.select("#sourceLevelWrapper").append("svg")
+        .attr("width", width)
+        .attr("height", height)
+        .attr("pointer-events", "all");
+
+    outer.append('rect')
+        .style('opacity',0)
+        .attr('width', "100%")
+        .attr('height', "100%")
+
+    var svg = outer
+        .append('g')
+        .attr('id', 'markers');*/
+
     d3.json('/artifacts/GLOBAL/'+encodeURIComponent(getParameterByName('graph')), function (error, graph) {
         var thisGraph = new Object();
         var jsonObj = JSON.parse(graph.graphicalGraph);
@@ -41,115 +59,119 @@ $(window).load(function() {
         thisGraph.edges = newEdges;
         console.log(thisGraph);
 
-        var width = $(window).width()*0.75;
-        var height = $(window).height()*0.75;
-        var nodeRadius = 7;
+        /*
+         * d3 v4 force graph: http://bl.ocks.org/fancellu/2c782394602a93921faff74e594d1bb1
+         */
 
-        outer = d3.select("#sourceLevelWrapper").append("svg")
-            .attr("width", width)
-            .attr("height", height)
-            .attr("pointer-events", "all");
+        var svg = d3.select("svg"),
+            width = +svg.attr("width"),
+            height = +svg.attr("height"),
+            node,
+            link;
 
-        outer.append('rect')
-            .style('opacity',0)
-            .attr('width', "100%")
-            .attr('height', "100%")
-            .call(d3.behavior.zoom().on("zoom", redraw))
+        svg.append('defs').append('marker')
+            .attr('id', 'arrowhead')
+            .attr('viewBox','-0 -5 10 10')
+            .attr('refX', 13)
+            .attr('refY', 0)
+            .attr('orient', 'auto')
+            .attr('markerWidth', 6)
+            .attr('markerHeight', 6)
+            .attr('xoverflow', 'visible')
+            .append('svg:path')
+            .attr('d', 'M 0,-5 L 10 ,0 L 0,5')
+            .attr('fill', '#aaa')
+            .style('stroke','none');
 
-        var svg = outer
-            .append('g')
-            .attr('id', 'markers');
+        var simulation = d3.forceSimulation()
+            .force("link", d3.forceLink().id(function (d) {return d.id;}).distance(100).strength(1))
+            .force("charge", d3.forceManyBody())
+            .force("center", d3.forceCenter(width / 2, height / 2));
 
-        function redraw() {
-            svg.attr("transform", "translate(" + d3.event.translate + ")" + " scale(" + d3.event.scale + ")");
-        }
 
-        force = cola.d3adaptor()
-            .linkDistance(75)
-            .size([width, height])
-            .avoidOverlaps(true);
+        var zoom_handler = d3.zoom()
+            .on("zoom", zoom_actions);
 
-        // A la release les arestes estan guardades com a "links", pero aquí són "edges"
-        force
-            .nodes(thisGraph.nodes)
-            .links(thisGraph.edges)
-            .jaccardLinkLengths(120,0.7)
-            .start(50);
+        zoom_handler(svg);
 
-        var defs = svg.append('svg:defs');
-
-        var marker = defs.selectAll('marker')
+        link = svg.selectAll(".link")
             .data(thisGraph.edges)
             .enter()
-            .append('svg:marker')
-            .attr('id', function(d){ return 'marker_' + d.name})
-            .attr('markerWidth', 7)
-            .attr('markerHeight', 7)
-            .attr('orient', 'auto')
-            .attr('refX', 6)
-            .attr('viewBox', '0 -5 10 10')
-            .append('svg:path')
-            .attr('d', 'M0,-5L10,0L0,5')
-            .attr('fill', '#aaa');
+            .append("line")
+            .attr("class", "link")
+            .attr('marker-end','url(#arrowhead)')
 
-        var path = svg.selectAll(".link")
+        link.append("title")
+            .text(function (d) {return d.name;});
+
+        edgepaths = svg.selectAll(".edgepath")
             .data(thisGraph.edges)
-            .enter().append('svg:path')
-            .attr('id', function(d,i) { return 'edgepath'+i; })
-            .attr('stroke', "#aaa")
-            .attr('marker-start', function(d,i){ return 'url(#marker_' + d.name + ')' })
-            .attr('marker-end', function(d,i){ return 'url(#marker_' + d.name  + ')' })
-            .attr('class', 'link');
+            .enter()
+            .append('path')
+            .attr('class', 'edgepath')
+            .attr('fill-opacity', 0)
+            .attr('stroke-opacity', 0)
+            .attr('id', function (d, i) {return 'edgepath' + i})
+            .style("pointer-events", "none");
 
-        var edgelabels = svg.selectAll(".edgelabel")
+        edgelabels = svg.selectAll(".edgelabel")
             .data(thisGraph.edges)
             .enter()
             .append('text')
             .style("pointer-events", "none")
-            .attr({'class':'edgelabel',
-                'id':function(d,i){return 'edgelabel'+i},
-                'dx':function(d,i){ return euclidean(d.source.x,d.source.y,d.target.x,d.target.y)/3-2*d.name.length;},
-                'dy':20,
-                'fill':'#aaa'});
+            .attr('class', 'edgelabel')
+            .attr('id', function (d, i) {return 'edgelabel' + i})
+            .attr('font-size', 10)
+            .attr('fill', '#aaa');
 
         edgelabels.append('textPath')
-            .attr('xlink:href',function(d,i) {return '#edgepath'+i})
+            .attr('fill', '#aaa')
+            .attr('xlink:href', function (d, i) {return '#edgepath' + i})
+            .style("text-anchor", "middle")
             .style("pointer-events", "none")
-            .text(function(d,i){ return d.name});
+            .attr("startOffset", "50%")
+            .text(function (d) {return d.name});
 
-        var node = svg.selectAll(".node")
+        node = svg.selectAll(".node")
             .data(thisGraph.nodes)
             .enter()
-            .append("circle")
+            .append("g")
+            .attr("class", "node")
+            .call(d3.drag()
+                    .on("start", dragstarted)
+                    .on("drag", dragged)
+                    .on("end", dragended)
+            );
+
+        node.append("circle")
             .attr("r", nodeRadius)
-            .attr("cx", 10)
-            .attr("cx", 10)
-            .style("fill", function(d) {
-                return d.color;
-            })
-            .call(force.drag);
+            .style("fill", function (d) {return d.color;})
 
-        var nodeTexts = svg.selectAll("text.label")
-            .data(thisGraph.nodes)
-            .enter().append("text")
-            .attr("class", "label")
-            .attr("value", function (d) {
-                return d.name;
-            })
-            .text(function (d) {
-                return d.name;
-            });
+        node.append("text")
+            .attr("dx", function(d) { return d.name.length/2 - 20})
+            .attr("dy", 30)
+            .attr('fill', '#000000')
+            .style("font-size", "10px")
+            .text(function (d) {return d.name;});
 
+        simulation
+            .nodes(thisGraph.nodes)
+            .on("tick", ticked);
 
-        force.on("tick", function () {
-            node.attr("cx", function (d) { return d.x; })
-                .attr("cy", function (d) { return d.y; });
+        simulation.force("link")
+            .links(thisGraph.edges);
 
-            nodeTexts.attr("transform", function (d) {
-                return "translate(" + (d.x - this.getBBox().width/2) + "," + (d.y + 25) + ")";
-            });
+        function ticked() {
+            link
+                .attr("x1", function (d) {return d.source.x;})
+                .attr("y1", function (d) {return d.source.y;})
+                .attr("x2", function (d) {return d.target.x;})
+                .attr("y2", function (d) {return d.target.y;});
 
-            path.attr('d', function (d) {
+            node
+                .attr("transform", function (d) {return "translate(" + d.x + ", " + d.y + ")";});
+
+            edgepaths.attr('d', function (d) {
                 var deltaX = d.target.x - d.source.x,
                     deltaY = d.target.y - d.source.y,
                     dist = Math.sqrt(deltaX * deltaX + deltaY * deltaY),
@@ -164,36 +186,57 @@ $(window).load(function() {
                 return 'M' + sourceX + ',' + sourceY + 'L' + targetX + ',' + targetY;
             });
 
-            edgelabels.attr('transform',function(d,i){
-                if (d.target.x<d.source.x){
-                    bbox = this.getBBox();
-                    rx = bbox.x+bbox.width/2;
-                    ry = bbox.y+bbox.height/2;
-                    return 'rotate(180 '+rx+' '+ry+')';
+            edgelabels.attr('transform', function (d) {
+                if (d.target.x < d.source.x) {
+                    var bbox = this.getBBox();
+
+                    rx = bbox.x + bbox.width / 2;
+                    ry = bbox.y + bbox.height / 2;
+                    return 'rotate(180 ' + rx + ' ' + ry + ')';
                 }
                 else {
                     return 'rotate(0)';
                 }
             });
+        }
 
-        });
+        function dragstarted(d) {
+            if (!d3.event.active) simulation.alphaTarget(0.3).restart()
+            d.fx = d.x;
+            d.fy = d.y;
+        }
 
-        /*
-            Based in Mike Bostock's Line drawing
-            https://bl.ocks.org/mbostock/f705fc55e6f26df29354
-         */
-       /* var line = d3.line();
+        function dragged(d) {
+            d.fx = d3.event.x;
+            d.fy = d3.event.y;
+        }
 
-        var svg = d3.select("#sourceLevelWrapper").append("svg")
-            .attr("width", $(window).width()*0.75)
-            .attr("height", $(window).height()*0.75)
-            .attr("pointer-events", "all")
-            .call(d3.drag()
-                .container(function() { return this; })
-                .subject(function() { var p = [d3.event.x, d3.event.y]; return [p, p]; })
-                .on("start", dragstarted));
+        function dragended(d) {
+            if (!d3.event.active) simulation.alphaTarget(0);
+            d.fx = null;
+            d.fy = null;
+        }
 
-        function dragstarted() {
+        function zoom_actions(){
+            svg.attr("transform", d3.event.transform)
+        }
+    });
+
+    /*
+        Based in Mike Bostock's Line drawing
+        https://bl.ocks.org/mbostock/f705fc55e6f26df29354
+     */
+    var line = d3.line();
+
+    var svg = d3.select("svg")
+        .attr("pointer-events", "all")
+        .call(d3.drag()
+            .container(function() { return this; })
+            .subject(function() { var p = [d3.event.x, d3.event.y]; return [p, p]; })
+            .on("start", dragstarted));
+
+    function dragstarted() {
+       // if (d3.event.shiftKey){
             var d = d3.event.subject,
                 active = svg.append("path").datum(d)
                     .attr("stroke", "#ff3f27")
@@ -212,102 +255,103 @@ $(window).load(function() {
                 else d[d.length - 1] = [x1, y1];
                 active.attr("d", line);
             });
-        }*/
+      //  }
+
+    }
 
 
-        /*
-            Based in Imad Boumzaoued's https://gist.github.com/RiseupDev/b07f7ccc1c499efc24e9
-         */
-      /*  var dragging = false, drawing = false, startPoint;
-        var points = [], g;
+    /*
+        Based in Imad Boumzaoued's https://gist.github.com/RiseupDev/b07f7ccc1c499efc24e9
+     */
+    /*  var dragging = false, drawing = false, startPoint;
+    var points = [], g;
     // behaviors
-        var dragger = d3.behavior.drag()
-            .on('drag', handleDrag)
-            .on('dragend', function(d){
-                dragging = false;
-            });
-        svg.on('mouseup', function(){
-            if(dragging) return;
-            drawing = true;
-            startPoint = [d3.mouse(this)[0], d3.mouse(this)[1]];
-            if(svg.select('g.drawPoly').empty()) g = svg.append('g').attr('class', 'drawPoly');
-            if(d3.event.target.hasAttribute('is-handle')) {
-                closePolygon();
-                return;
-            };
-            points.push(d3.mouse(this));
-            g.select('polyline').remove();
-            var polyline = g.append('polyline').attr('points', points)
-                .style('fill', 'none')
-                .attr('stroke', '#000');
-            for(var i = 0; i < points.length; i++) {
-                g.append('circle')
-                    .attr('cx', points[i][0])
-                    .attr('cy', points[i][1])
-                    .attr('r', 4)
-                    .attr('fill', 'yellow')
-                    .attr('stroke', '#000')
-                    .attr('is-handle', 'true')
-                    .style({cursor: 'pointer'});
-            }
+    var dragger = d3.behavior.drag()
+        .on('drag', handleDrag)
+        .on('dragend', function(d){
+            dragging = false;
         });
-        function closePolygon() {
-            svg.select('g.drawPoly').remove();
-            var g = svg.append('g');
-            g.append('polygon')
-                .attr('points', points)
-                .style('fill', getRandomColor());
-            for(var i = 0; i < points.length; i++) {
-                var circle = g.selectAll('circles')
-                    .data([points[i]])
-                    .enter()
-                    .append('circle')
-                    .attr('cx', points[i][0])
-                    .attr('cy', points[i][1])
-                    .attr('r', 4)
-                    .attr('fill', '#FDBC07')
-                    .attr('stroke', '#000')
-                    .attr('is-handle', 'true')
-                    .style({cursor: 'move'})
-                    .call(dragger);
-            }
-            points.splice(0);
-            drawing = false;
+    svg.on('mouseup', function(){
+        if(dragging) return;
+        drawing = true;
+        startPoint = [d3.mouse(this)[0], d3.mouse(this)[1]];
+        if(svg.select('g.drawPoly').empty()) g = svg.append('g').attr('class', 'drawPoly');
+        if(d3.event.target.hasAttribute('is-handle')) {
+            closePolygon();
+            return;
+        };
+        points.push(d3.mouse(this));
+        g.select('polyline').remove();
+        var polyline = g.append('polyline').attr('points', points)
+            .style('fill', 'none')
+            .attr('stroke', '#000');
+        for(var i = 0; i < points.length; i++) {
+            g.append('circle')
+                .attr('cx', points[i][0])
+                .attr('cy', points[i][1])
+                .attr('r', 4)
+                .attr('fill', 'yellow')
+                .attr('stroke', '#000')
+                .attr('is-handle', 'true')
+                .style({cursor: 'pointer'});
         }
-        svg.on('mousemove', function() {
-            if(!drawing) return;
-            var g = d3.select('g.drawPoly');
-            g.select('line').remove();
-            var line = g.append('line')
-                .attr('x1', startPoint[0])
-                .attr('y1', startPoint[1])
-                .attr('x2', d3.mouse(this)[0] + 2)
-                .attr('y2', d3.mouse(this)[1])
-                .attr('stroke', '#53DBF3')
-                .attr('stroke-width', 1);
-        })
-        function handleDrag() {
-            if(drawing) return;
-            var dragCircle = d3.select(this), newPoints = [], circle;
-            dragging = true;
-            var poly = d3.select(this.parentNode).select('polygon');
-            var circles = d3.select(this.parentNode).selectAll('circle');
-            dragCircle
-                .attr('cx', d3.event.x)
-                .attr('cy', d3.event.y);
-            for (var i = 0; i < circles[0].length; i++) {
-                circle = d3.select(circles[0][i]);
-                newPoints.push([circle.attr('cx'), circle.attr('cy')]);
-            }
-            poly.attr('points', newPoints);
-        }
-        function getRandomColor() {
-            var letters = '0123456789ABCDEF'.split('');
-            var color = '#';
-            for (var i = 0; i < 6; i++) {
-                color += letters[Math.floor(Math.random() * 16)];
-            }
-            return color;
-        }*/
     });
+    function closePolygon() {
+        svg.select('g.drawPoly').remove();
+        var g = svg.append('g');
+        g.append('polygon')
+            .attr('points', points)
+            .style('fill', getRandomColor());
+        for(var i = 0; i < points.length; i++) {
+            var circle = g.selectAll('circles')
+                .data([points[i]])
+                .enter()
+                .append('circle')
+                .attr('cx', points[i][0])
+                .attr('cy', points[i][1])
+                .attr('r', 4)
+                .attr('fill', '#FDBC07')
+                .attr('stroke', '#000')
+                .attr('is-handle', 'true')
+                .style({cursor: 'move'})
+                .call(dragger);
+        }
+        points.splice(0);
+        drawing = false;
+    }
+    svg.on('mousemove', function() {
+        if(!drawing) return;
+        var g = d3.select('g.drawPoly');
+        g.select('line').remove();
+        var line = g.append('line')
+            .attr('x1', startPoint[0])
+            .attr('y1', startPoint[1])
+            .attr('x2', d3.mouse(this)[0] + 2)
+            .attr('y2', d3.mouse(this)[1])
+            .attr('stroke', '#53DBF3')
+            .attr('stroke-width', 1);
+    })
+    function handleDrag() {
+        if(drawing) return;
+        var dragCircle = d3.select(this), newPoints = [], circle;
+        dragging = true;
+        var poly = d3.select(this.parentNode).select('polygon');
+        var circles = d3.select(this.parentNode).selectAll('circle');
+        dragCircle
+            .attr('cx', d3.event.x)
+            .attr('cy', d3.event.y);
+        for (var i = 0; i < circles[0].length; i++) {
+            circle = d3.select(circles[0][i]);
+            newPoints.push([circle.attr('cx'), circle.attr('cy')]);
+        }
+        poly.attr('points', newPoints);
+    }
+    function getRandomColor() {
+        var letters = '0123456789ABCDEF'.split('');
+        var color = '#';
+        for (var i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+    }*/
 });
