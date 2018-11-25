f26d999b8db17d68cf3990fe4a7a4688fad760a4
serginf <snadal@essi.upc.edu>
2018-11-25 10:24:38 +0100

    add timeout to experiments runner

--- MetadataStorage/experimentsRunner.sh
+++ MetadataStorage/experimentsRunner.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-for i in `seq 2 10`
+for i in `seq 2 7`
 do
 	for j in `seq 2 25`
 	do
-		java -jar build/libs/MetadataStorage-0.0.1-SNAPSHOT.jar 50 5 $i $j 0.15 0.75
-                java -jar build/libs/MetadataStorage-0.0.1-SNAPSHOT.jar 50 5 $i $j 0.15 0.75
-                java -jar build/libs/MetadataStorage-0.0.1-SNAPSHOT.jar 50 5 $i $j 0.15 0.75
+		timeout 10m java -jar build/libs/MetadataStorage-0.0.1-SNAPSHOT.jar 50 5 $i $j 0.15 0.75
+                timeout 10m java -jar build/libs/MetadataStorage-0.0.1-SNAPSHOT.jar 50 5 $i $j 0.15 0.75
+                timeout 10m java -jar build/libs/MetadataStorage-0.0.1-SNAPSHOT.jar 50 5 $i $j 0.15 0.75
 	done
 done
