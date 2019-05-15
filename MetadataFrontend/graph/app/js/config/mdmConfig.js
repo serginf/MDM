aabac26977079f236d07ec811304f81cb51c0354
Javier Flores <javierfh1404@gmail.com>
2019-05-16 00:02:13 +0200

    add configuration for omq mode

--- MetadataFrontend/graph/app/js/config/mdmConfig.js
+++ MetadataFrontend/graph/app/js/config/mdmConfig.js
@@ -1,40 +1,53 @@
 module.exports = function () {
-    var options = {},
+    var config = {},
         global_Graph_Edit={
             editorMode : "true",
-            selectSG   : "false"
+            selectSG   : "false",
+            OMQ_mode   : "false"
         },
         mappings_Graph_select = {
             editorMode : "false",
-            selectSG   : "true"
+            selectSG   : "true",
+            OMQ_mode   : "false"
+        },
+        omq = {
+            editorMode : "false",
+            selectSG   : "false",
+            OMQ_mode   : "true"
         },
         default_Option = {
             editorMode : "false",
-            selectSG   : "false"
+            selectSG   : "false",
+            OMQ_mode   : "false"
         };
 
-    options.getConf=function (type) {
+    config.getConf=function (type) {
         switch (type) {
             case "mappings_Graph_select":
                 return mappings_Graph_select;
             case "global_Graph_Edit":
                 return global_Graph_Edit;
+            case "omq":
+                return omq;
             default:
                 return default_Option;
         }
         return global_Graph_Edit;
     };
 
-    options.global_Graph_Edit=function () {
+    config.global_Graph_Edit=function () {
         return global_Graph_Edit;
     };
-    options.mappings_Graph_select=function () {
+    config.mappings_Graph_select=function () {
         return mappings_Graph_select;
     };
-    options.default_Option=function () {
+    config.default_Option=function () {
         return default_Option;
     };
+    config.omq = function () {
+        return omq;
+    };
 
-    return options;
+    return config;
 
 };
