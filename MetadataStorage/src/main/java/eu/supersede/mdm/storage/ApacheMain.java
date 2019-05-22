62ddac7eb40b0bd4af98b0d2ca2db4703c08f8d4
Javier Flores <javierfh1404@gmail.com>
2019-05-23 01:21:21 +0200

    add swagger and swagger-ui

--- MetadataStorage/src/main/java/eu/supersede/mdm/storage/ApacheMain.java
+++ MetadataStorage/src/main/java/eu/supersede/mdm/storage/ApacheMain.java
@@ -1,11 +1,20 @@
 package eu.supersede.mdm.storage;
 
+import io.swagger.jaxrs.config.BeanConfig;
+import io.swagger.jaxrs.listing.ApiListingResource;
+import io.swagger.jaxrs.listing.SwaggerSerializers;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.ContextHandler;
+import org.eclipse.jetty.server.handler.ContextHandlerCollection;
+import org.eclipse.jetty.server.handler.ResourceHandler;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.servlet.ServletContainer;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.logging.LogManager;
 /**
  * Created by snadal on 6/06/17.
  */
@@ -21,10 +30,23 @@ public class ApacheMain {
 
         ResourceConfig config = new ResourceConfig();
         config.packages("eu.supersede.mdm.storage.resources");
+        config.packages("eu.supersede.mdm.storage.errorhandling");
+        config.register(ApiListingResource.class);
+        config.register(SwaggerSerializers.class);
+
+        configSwagger();
+
         ServletHolder servlet = new ServletHolder(new ServletContainer(config));
         Server server = new Server(8082);
+
         ServletContextHandler context = new ServletContextHandler(server, "/*");
         context.addServlet(servlet, "/*");
+
+        ContextHandlerCollection contexts = new ContextHandlerCollection();
+        contexts.addHandler(buildSwaggerUI());
+        contexts.addHandler(context);
+
+        server.setHandler(contexts);
         try {
             server.start();
             server.join();
@@ -32,4 +54,36 @@ public class ApacheMain {
             e.printStackTrace();
         }
     }
+
+    // http://localhost:8082/swagger.json
+    public static void  configSwagger() {
+        BeanConfig beanConfig = new BeanConfig();
+        beanConfig.setVersion("1");
+        beanConfig.setTitle("MetadataStorage");
+        beanConfig.setDescription("Description");
+        beanConfig.setSchemes(new String[]{"http"});
+        beanConfig.setHost("localhost:8082");
+        beanConfig.setBasePath("/");
+        beanConfig.setResourcePackage("eu.supersede.mdm.storage.resources");
+        beanConfig.setPrettyPrint(true);
+        beanConfig.setScan(true);
+    }
+
+
+    // This starts the Swagger UI at http://localhost:8082/docs/
+    private static ContextHandler buildSwaggerUI() throws Exception {
+        ResourceHandler rh = new ResourceHandler();
+        //  .getResource("META-INF/resources/webjars/swagger-ui/3.20.9")
+        rh.setResourceBase(ApacheMain.class.getClassLoader()
+                .getResource("swagger-ui")
+                .toURI().toString());
+
+        ContextHandler context = new ContextHandler();
+        context.setContextPath("/docs/");
+        context.setWelcomeFiles(new String[] { "index.html" });
+        context.setHandler(rh);
+        return context;
+    }
+
+    
 }
