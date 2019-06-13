836399ec9284a578b4b6a0e0fa0706642a8ef6a7
Kashif-Rabbani <ch.kashirabbani@gmail.com>
2019-06-13 14:45:36 +0200

    Data Sources frontend bug resolved for number of wrappers

--- MetadataFrontend/public/js/client_manage_data_sources.js
+++ MetadataFrontend/public/js/client_manage_data_sources.js
@@ -8,6 +8,10 @@ function getDataSources() {
         var i = 1;
         $.each((data), function(key, value) {
             var dataSource = JSON.parse(value);
+            var wrappersLength = 0;
+            if(dataSource.wrappers){
+                wrappersLength = dataSource.wrappers.length;
+            }
             $('#dataSources').find('tbody')
                 .append($('<tr>')
                         .append($('<td>')
@@ -17,7 +21,7 @@ function getDataSources() {
                         ).append($('<td>')
                              .text(dataSource.type)
                         ).append($('<td>')
-                            .text(dataSource.wrappers.length)
+                            .text(wrappersLength)
                     ).append($('<td>').append($('<a href="/view_data_source?dataSourceID='+(dataSource.dataSourceID)+'">').append($('<span class="fa fa-search"></span>')))
                     ).append($('<td>').append($('<a href="/view_source_graph?iri='+(dataSource.iri)+'">').append($('<span class="fa fa-search"></span>')))
                     )
